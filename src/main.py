6d45227d66b76c7ffd1220b668b15acfb1482bc8
DarKev03 <kevingarciah03@gmail.com>
2024-11-22 11:50:13 +0100

    MAs cambios

--- src/main.py
+++ src/main.py
@@ -1,22 +1,17 @@
 from google_calendar import create_google_calendar_event, get_google_calendar_service, is_task_in_google_calendar
 from notion import get_notion_tasks
 
-# Sincronizar tareas de Notion con Google Calendar
 def sync_notion_with_google_calendar():
-    """
-    Función principal que sincroniza las tareas de Notion con Google Calendar.
-    """
     tasks = get_notion_tasks()
     service = get_google_calendar_service()
     for task in tasks:
         if not is_task_in_google_calendar(service, task['name'], task['date']):
             create_google_calendar_event(service, task)
 
-# Handler para Vercel
 def handler(request):
     """
-    Handler compatible con Vercel. 
-    Ejecuta la sincronización y devuelve un mensaje de confirmación.
+    Handler principal para Vercel.
+    Este método se ejecutará cada vez que se invoque la función en Vercel.
     """
     try:
         sync_notion_with_google_calendar()
