e234c0638ada4471a91fa592cf6c42de0905a9ae
DarKev03 <kevingarciah03@gmail.com>
2024-11-22 13:36:54 +0100

    Se añade la funcionalidad de que en segundo plano sincronice las tareas

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
+from threading import Timer, Thread
 from flask import Flask, jsonify, request
 from src.google_calendar import create_google_calendar_event, get_google_calendar_service, is_task_in_google_calendar
 from src.notion import get_notion_tasks
-
 
 app = Flask(__name__)
 
@@ -11,8 +11,8 @@ def home():
 
 @app.route("/sync", methods=["GET"])
 def sync():
+    """Sincronización manual activada desde el endpoint."""
     try:
-        # Sincronizar Notion con Google Calendar
         tasks = get_notion_tasks()
         service = get_google_calendar_service()
 
@@ -24,5 +24,25 @@ def sync():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
+def periodic_sync():
+    """Función que ejecuta la sincronización periódica."""
+    print("Iniciando sincronización automática...")
+    try:
+        tasks = get_notion_tasks()
+        service = get_google_calendar_service()
+
+        for task in tasks:
+            if not is_task_in_google_calendar(service, task['name'], task['date']):
+                create_google_calendar_event(service, task)
+
+        print("Sincronización automática completada.")
+    except Exception as e:
+        print(f"Error en la sincronización automática: {e}")
+
+    # Programar la próxima ejecución
+    Timer(60 * 60 * 5, periodic_sync).start()  # 5 horas
+
 if __name__ == "__main__":
+    # Inicia la sincronización automática en un hilo separado
+    Thread(target=periodic_sync, daemon=True).start()
     app.run(debug=True)
